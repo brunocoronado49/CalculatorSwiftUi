c54abba8b416a2c74046e74f63e3d010d808241a
brunocoronado49 <francorangelcoronado@gmail.com>
2024-05-27 00:30:47 -0600

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Bruno  on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
